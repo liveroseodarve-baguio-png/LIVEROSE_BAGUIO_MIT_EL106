c6c112b8a478882031231f3e03b32615d35fc0e8
liveroseodarve-baguio-png <liverose.odarve-baguio@ustp.edu.ph>
2025-10-28 07:46:27 +0800

    Add files via upload

---
+++ config/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/', include('users.urls')),
+    path('', include('users.urls')),  # ✅ This connects all routes from users app
+]
